a4aa708a44e77e44690977afd6abace69593c439
Advait55555 <advaitduvvuru21@gmail.com>
2026-02-05 09:19:39 +0530

    ok

--- lec3.py
+++ lec3.py
@@ -1,4 +1,4 @@
-#decimal to b
+# decimal to b
 # Function to convert decimal to binary
 # def decimal_to_binary(decimal_number):
 #     binary_number = []
@@ -29,17 +29,17 @@
 # print(f"a or b{c}")
 # print(f"a n b {d} ")
 
-#to check if number is even or odd
+# to check if number is even or odd
 
-#if a & (a-1) == 0 ; number can be written as a power of 2
+# if a & (a-1) == 0 ; number can be written as a power of 2
 
-#left and right shift
+# left and right shift
 # a = 13
 # print(f"left shift: {a << 1}")
 # print(f"right shift: {a>>1}")
 
-#swapping w/o temp var
-#first with temp:
+# swapping w/o temp var
+# first with temp:
 # a = 5
 # b = 7
 # print("before swapping:")
@@ -72,8 +72,8 @@
 # a = a^b
 # print(a,b)
 
-#home work ques
-#1.using bitwise operator check if num divis by 4 or not
+# home work ques
+# 1.using bitwise operator check if num divis by 4 or not
 
 # a = 15
 # if a & 3 == 0:
@@ -81,7 +81,7 @@
 # else:
 #     print("no")
 
-#divide num by 2 and reminder n quotient 
+# divide num by 2 and reminder n quotient 
 
 # print(11 >> 1)
 # print(11 & 1)
@@ -92,7 +92,7 @@
 # else:
 #     print(f"loss by {cost-sp}rupees")
 
-#ques 3
+# ques 3
 # x1 = int(input("enter x1:"))
 # y1 = int(input("enter y1:"))
 # x2 = int(input("enter x2:"))
@@ -108,7 +108,7 @@
 # else:
 #     print("They do not lie on a straight line")
 
-#ques 2
+# ques 2
 # n1 = int(input("Enter n1 :"))
 # o1 = input("ENter o1 :")
 # n2 = int(input("Enter n2 :"))
@@ -144,33 +144,33 @@
 # else:
 #     print("invalid operator")
 #     exit()
-hardness = int(input("Enter the hardness of steel:"))
-cc = float(input("Enter carbon content:"))
-ts = int(input("Enter tensile strength:"))
-if hardness > 50:
-    i = 1
-else:
-    i = 0
-if cc < 0.7:
-    ii = 1
-else:
-    ii = 0
-if ts > 5600:
-    iii = 1
-else:
-    iii = 0
-if i == 1 and ii == 1 and iii == 1:
-    print("grade 10")
-elif i == 1 and ii == 1 and iii == 0:
-    print("grade 9") 
-elif i == 0 and ii == 1 and iii == 1:
-    print("grade 8")
-elif i == 1 and ii ==0 and iii == 1:
-    print("grade 7")
-elif i == 1 or ii == 1 or iii == 1:
-    print("grade 6") 
-elif i == 0 or ii == 0 or iii == 0:
-    print("grade 5")
-else:
-    print("invalid input")
-    exit()
+# hardness = int(input("Enter the hardness of steel:"))
+# cc = float(input("Enter carbon content:"))
+# ts = int(input("Enter tensile strength:"))
+# if hardness > 50:
+#     i = 1
+# else:
+#     i = 0
+# if cc < 0.7:
+#     ii = 1
+# else:
+#     ii = 0
+# if ts > 5600:
+#     iii = 1
+# else:
+#     iii = 0
+# if i == 1 and ii == 1 and iii == 1:
+#     print("grade 10")
+# elif i == 1 and ii == 1 and iii == 0:
+#     print("grade 9") 
+# elif i == 0 and ii == 1 and iii == 1:
+#     print("grade 8")
+# elif i == 1 and ii ==0 and iii == 1:
+#     print("grade 7")
+# elif i == 1 or ii == 1 or iii == 1:
+#     print("grade 6") 
+# elif i == 0 or ii == 0 or iii == 0:
+#     print("grade 5")
+# else:
+#     print("invalid input")
+#     exit()
