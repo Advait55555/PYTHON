701fe11efeb64901822b392a76ef9c470dd4589d
Advait55555 <advaitduvvuru21@gmail.com>
2026-02-06 11:48:52 +0530

    more more cclout clout

--- lec5.py
+++ lec5.py
@@ -14,11 +14,26 @@
 #         ss =ss+i
 # print(ss)
 
-a = int(input("enter an integer:"))
-sum = 0
-while a!=0:
-    lastdigit = a%10
-    sum = sum + (lastdigit**2)
-    a = a//10
-print(sum)
+# a = int(input("enter an integer:"))
+# sum = 0
+# while a!=0:
+#     lastdigit = a%10
+#     sum = sum + (lastdigit**2)
+#     a = a//10
+# print(sum)
+
+# a = 0
+# while a != 1:
+#     a = int(input("enter number:"))
+
+# s = input("Enter a string:")
+# search = input("Enter the letter you want to search:")
+# count = 0
+# for i in s:
+#     count += 1 
+# for i in range(0,count):
+#     if s[i] == search:
+#         print("found at index number",i)
+#         break
+#use flag variable for element not found
 
