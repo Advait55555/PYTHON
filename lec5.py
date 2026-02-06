55cd38fa5963924153d5b55cc9551e3025f87ca8
Advait55555 <advaitduvvuru21@gmail.com>
2026-02-06 11:06:32 +0530

    more clout

--- lec5.py
+++ lec5.py
@@ -1,5 +1,15 @@
-a = "advait"
-i = 0 
-while a != "\0":
-    print(a[i])
-    i += 1
+# a = "advait"
+# i = 0 
+# while a != "\0":
+#     print(a[i])
+#     i += 1
+#^shows error 
+
+s = input("Enter a string in lower case")
+ss = ""
+for i in s:
+    if ord(i)<=ord('z') and ord(i)>=ord('a'):
+        ss = ss+chr(ord(i)-32)
+    else: 
+        ss =ss+i
+print(ss)
