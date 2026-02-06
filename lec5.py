fcbcc6d1199d929b30e311073a00f077e6bd959d
Advait55555 <advaitduvvuru21@gmail.com>
2026-02-06 11:14:18 +0530

    mroe clout

--- lec5.py
+++ lec5.py
@@ -5,11 +5,18 @@
 #     i += 1
 #^shows error 
 
-s = input("Enter a string in lower case")
-ss = ""
-for i in s:
-    if ord(i)<=ord('z') and ord(i)>=ord('a'):
-        ss = ss+chr(ord(i)-32)
-    else: 
-        ss =ss+i
-print(ss)
+# s = input("Enter a string in lower case")
+# ss = ""
+# for i in s:
+#     if ord(i)<=ord('z') and ord(i)>=ord('a'):
+#         ss = ss+chr(ord(i)-32)
+#     else: 
+#         ss =ss+i
+# print(ss)
+
+a = int(input("enter an integer:"))
+count = 0
+while a!=0:
+    a = a//10
+    count += 1
+print(count)
